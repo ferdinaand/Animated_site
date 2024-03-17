b9cdb3b7b5adbc396566eb66e9a49edeb0776593
ferdinaand <ekpo546@gmail.com>
2024-03-17 23:33:47 +0100

    portfolio--update

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+import { motion } from 'framer-motion';
+
+const ContactPage = () => {
+return (
+  <motion.div lassName='h-full' initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration:1}}>  
+<div className= ''>ContactPage</div>
+</motion.div>
+);
+}
+
+export default ContactPage
