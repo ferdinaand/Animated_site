9fc5ca4db0a922641824eb4e56cd6f2235a9b2ef
ferdinaand <ekpo546@gmail.com>
2024-04-08 23:26:12 +0100

    update

--- src/components/navLink.jsx
+++ src/components/navLink.jsx
@@ -7,12 +7,12 @@ import { usePathname } from "next/navigation";
 // the NavLink accepts the Link prop, we use this to:
 // 1. check if the Link prop is the same as the pathName
 // 2. if it's true, we add a background to the navbar link to indicate that it's selected
-const NavLink = ({link}) => {
-const pathName = usePathname()
-console.log(pathName);
-return (
-<Link className={`rounded p-1 ${pathName === link.url && "bg-black text-white"}`} href={link.url}>{link.title}</Link>
-)
+const NavLink = ({ link }) => {
+    const pathName = usePathname()
+    console.log(pathName);
+    return (
+        <Link className={`rounded p-2 text-white ${pathName === link.url && "bg-black text-white"}`} href={link.url}>{link.title}</Link>
+    )
 }
 
 export default NavLink
