ea81de05c1315fae4ca5db5e19362ebc50b54838
ferdinaand <ekpo546@gmail.com>
2024-04-08 23:25:55 +0100

    update

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,12 +1,18 @@
 "use client";
-import { motion } from 'framer-motion';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { useRef } from 'react';
 
 const PortfolioPage = () => {
-return (
-  <motion.div lassName='h-full' initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration:1}}>  
-<div className= ''>PortfolioPage</div>
-</motion.div>
-);
+
+  const ref = useRef()
+  const { scrollYProgress } = useScroll({ container: ref })
+
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "100%"])
+  return (
+    <motion.div lassName='h-full' initial={{ y: "-200vh" }} animate={{ y: "0%" }} transition={{ duration: 1 }}>
+      <div className=' h-{600vh}' ref={ref}>PortfolioPage</div>
+    </motion.div>
+  );
 }
 
 export default PortfolioPage
